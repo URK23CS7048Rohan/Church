bfba5b0a811efd0241bb924a78583613325b84c7
URK23CS7048Rohan <rohanchowdhary@karunya.edu.in>
2026-06-15 11:23:06 +0530

    fix: set capacitor production URL

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,8 +5,7 @@ const config: CapacitorConfig = {
   appName: 'Sanctuary',
   webDir: 'out',
   server: {
-    // Set this to your local dev computer's IP (e.g. 'http://192.168.1.50:3000') or production site URL.
-    url: 'http://192.168.1.40:3000',
+    url: 'https://agapeinternational.vercel.app',
     cleartext: true
   }
 };
